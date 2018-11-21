c15533afe06b504b5cc32a8e7bd3c7204af2b046
Gabriel Vidal <golobar2012@gmail.com>
2018-11-21 19:34:33 +0100

    @migueldavidorrico ya está hecho, a mi me funciona, ¿podrías comprobar si está bien?

--- src/PiramideInvertida.java
+++ src/PiramideInvertida.java
@@ -1,11 +1,27 @@
 public class PiramideInvertida {
     public static void main(String[] args) {
         int filas=5;
+
         System.out.println(cadenaPiramideInvertida(filas));
     }
 
     public static String cadenaPiramideInvertida(int filas) {
+        String salida = "";
+        int ancho=(filas-1)*2+1;
+        int espaciosIniciales=0;
 
-        return null;
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < espaciosIniciales; j++) {
+                salida+=" ";
+            }
+            for (int j = 0; j < ancho; j++) {
+                salida+="*";
+            }
+            salida+="\n";
+            ancho=ancho-2;
+            espaciosIniciales++;
+        }
+
+        return salida;
     }
 }
